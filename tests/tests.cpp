69cbe2c22559c683eab29be006fb22f47fcf1053
Thomas Aubert <aubert.thms@gmail.com>
2024-02-22 21:18:57 +0100

    first working prototype for unit tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hregergello fromsfgsfg Fenix!" << std::endl;
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include "../include/ull.h"
+
+
+TEST_CASE("basic ull tests") {
+
+    struct Element : ull::Node<Element> {
+        int value;
+    private:
+        // only for size checks
+        char dummyBuffer[64];
+    };
+
+    ull::List<Element> list;
+
+    CHECK(list.empty());
+    CHECK(list.size() == 0);
+    CHECK(sizeof(list) == 4 * sizeof(uintptr_t));
+
+    Element e1;
+    Element e2;
+    Element e3;
+    Element e4;
+
+    list.push_back(e1);
+
 }
+
